3a4b6f6ea2df96b4c96b81cf70db0ce89b52f32e
Andrew Chilton <chilts@appsattic.com>
2012-05-04 21:08:58 +1200

    Finished version of connect-stream-s3

--- connect-stream-s3.js
+++ connect-stream-s3.js
@@ -11,6 +11,7 @@
 
 var fs = require('fs');
 
+var async = require('async');
 var awssum = require('awssum');
 var amazon = awssum.load('amazon/amazon');
 var s3Service = awssum.load('amazon/s3');
@@ -23,33 +24,58 @@ module.exports = function(options) {
     var secretAccessKey = options.secretAccessKey;
     var awsAccountId    = options.awsAccountId;
     var region          = options.region;
+    var bucketName      = options.bucketName;
+    var concurrency     = options.concurrency || 3;
 
     // create the S3 API
     var s3 = new s3Service(accessKeyId, secretAccessKey, awsAccountId, region);
 
     return function handler(req, res, next) {
-        // ToDo: FOR ALL THE FILES!!!
+        // remember what happened to each of these files
+        var allOk = true;
 
-        // open the file as a read stream
-        var bodyStream = fs.createReadStream( req.files.file_1.path );
+        // create an async function to upload the file
+        var upload = function(fieldname, callback) {
+            // open the file as a read stream
+            var bodyStream = fs.createReadStream( req.files[fieldname].path );
 
-        var options = {
-            BucketName    : 'pie-18',
-            ObjectName    : req.files.file_1.name,
-            ContentLength : req.files.file_1.size,
-            Body          : bodyStream,
-        };
+            var options = {
+                BucketName    : bucketName,
+                ObjectName    : req.files[fieldname].name,
+                ContentLength : req.files[fieldname].size,
+                Body          : bodyStream,
+            };
 
-        s3.PutObject(options, function(err, data) {
-            if (err) {
-                next(err);
-                return;
+            s3.PutObject(options, function(err, data) {
+                // remember what happened
+                req.files[fieldname].s3 = {
+                    'err'  : err,
+                    'data' : data,
+                };
+                if (err) {
+                    allOk = false;
+                } // else, everything was ok
+                // tell the queue we're finished
+                callback();
+            });
+        }
+
+        // create a queue so we can do all of the uploaded files to S3
+        var q = async.queue(upload, concurrency);
+
+        // now, add all of these fields onto the queue
+        for(var fieldname in req.files) {
+            q.push(fieldname);
+        }
+
+        q.drain = function() {
+            if ( allOk ) {
+                next();
             }
-            // all ok
-            // console.log(data);
-            req.files.file_1.s3 = true;
-            next();
-        });
+            else {
+                next('One or more of the files failed to upload to S3.');
+            }
+        };
     };
 };
 
